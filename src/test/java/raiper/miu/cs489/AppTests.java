1831ca42ca8773eae5a8d14c5919e4e2433913e8
raipereira <raipereira84@gmail.com>
2024-04-27 15:47:56 -0500

    Fist commit of backend ecomerce project

---
+++ src/test/java/raiper/miu/cs489/AppTests.java
@@ -0,0 +1,13 @@
+package raiper.miu.cs489;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
